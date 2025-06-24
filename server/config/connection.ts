cd16307621c250645d3d155a1096626c6720b83c
Chartok <Chartok@users.noreply.github.com>
2025-06-24 00:20:26 -0500

    updated mongoose to log successful connection

--- server/config/connection.ts
+++ server/config/connection.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 mongoose.connect(
-	process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search'
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search',
+	console.log('MongoDB connected successfully')
 );
 
 export default mongoose.connection;
