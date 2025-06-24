7ffc63d7c7d512d7f2ab26b5b0bcd51bac88a5a8
Chartok <Chartok@users.noreply.github.com>
2025-06-23 23:41:03 -0500

    migrating from apollo 3 to apollo 4

--- server/server.ts
+++ server/server.ts
@@ -1,38 +1,48 @@
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@as-integrations/express4';
 import express from 'express';
-import path from 'path';
-import { ApolloServer } from 'apollo-server';
+import http from 'http';
+import cors from 'cors';
 import db from './config/connection';
 import { typeDefs, resolvers } from './schemas';
 import { authMiddleware } from './utils/auth';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const httpServer = http.createServer(app);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-const server = new ApolloServer({
+const server = new ApolloServer<authMiddleware>({
 	typeDefs,
 	resolvers,
-	context: authMiddleware,
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-async function startApollo() {
-	await server.start();
-	server.applyMiddleware({ app });
+await server.start();
+app.use(
+	'/graphql',
+	cors<cors.CorsRequest>(),
+	express.json(),
+	expressMiddleware(server, {
+		context: authMiddleware,
+	})
+);
 
-	if (process.env.NODE_ENV === 'production') {
-		app.use(express.static(path.join(__dirname, '../client/dist')));
-	}
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-	});
-
-	db.once('open', () => {
-		app.listen(PORT, () => {
-			console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`);
-		});
-	});
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/dist')));
 }
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
-startApollo();
+db.once('open', () => {
+	app.listen(PORT, () => {
+		console.log(`🌍 Now listening on port ${PORT}/graphql!`);
+	});
+});
+
+await new Promise<void>((resolve) =>
+	httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
+);
+console.log(
+	`🚀 Server ready at http://localhost:${process.env.PORT || 4000}/graphql`
+);
