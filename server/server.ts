1b7e79e8cb03218891bafc7dc0627d5bbee1cda2
Chartok <Chartok@users.noreply.github.com>
2025-06-24 11:12:18 -0500

    refined jwt, apollo-server logic

--- server/server.ts
+++ server/server.ts
@@ -1,30 +1,19 @@
 import { ApolloServer } from 'apollo-server-express';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import path from 'path';
-import db from './config/connection';
 import { typeDefs, resolvers } from './schemas';
 import { authMiddleware } from './utils/auth';
 
-interface MyContext {
-    token?: string;
-}
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
-const server = new ApolloServer<MyContext>({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
-    context: async ({ req }) => authMiddleware,
-    listen: { port: 4000 },
+    context:  ({ req }) => authMiddleware({ req }),
+    listen: { port: PORT },
 });
 
 console.log(`Apollo/Server ready @${url}`);
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`);
-    });
-  });
-}
 
 startApollo();
--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -6,21 +6,22 @@ const expiration = '2h';
 export function signToken({ username, email, _id }) {
 	const payload = { username, email, _id };
 	return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-};
+}
 
-export const authMiddleware = async ({ req }) => {
-    let user = null;
-    let token = req.headers.authorization || '';
-    if (token) {
-        token = token.replace('Bearer', '').trim();
-        try {
-            const { data } = jwt.verify(token, secret, { maxAge: expiration });
-            user = data;
-        } catch (error) {
-            console.log('token error; invalid or expired');
-            user = null;
-        }
-    }
+export const authMiddleware = ({ req }: { req: Request }) => {
+	//    let user = null;
+	let token = (req.headers.authorization || '').replace(/^Bearer\s+/, '');
+	if (token) {
+		token = token.replace('Bearer', '').trim();
+		try {
+			const { data } = jwt.verify(token, secret) as { data: JwtPayload };
+            console.log('token verified');
+			return { user: data };
+		} catch {
+			console.log('token error; invalid or expired');
+			return { user: null };
+		}
+	};
 
-    return { user };
+	return { user };
 };
