0f3cdd3861115e04460f4164ec58c687f64500f0
Chartok <Chartok@users.noreply.github.com>
2025-06-24 14:44:54 -0500

    added mongodb-related dependencies

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs, resolvers } from './schemas';
+import { connectToDatabase } from './config/connection';
 import { authMiddleware } from './utils/auth';
 
 const PORT = process.env.PORT;
@@ -9,6 +10,7 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
 	context: ({ req }) => authMiddleware({ req }),
+  
 	listen: { port: PORT },
 });
 
