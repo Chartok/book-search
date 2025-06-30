97e93f2d9a0acb653473e90462f8ae2fcf09ad5f
Chartok <Chartok@users.noreply.github.com>
2025-06-30 02:28:28 -0500

    getting ready to sequelize; corrected enviroment variables and calls

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -3,7 +3,11 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_URI as string, {
+export const sequelize = new Sequelize(process.env.DB_NAME as string, {
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	dialect: 'mysql',
 	logging: false,
 });
