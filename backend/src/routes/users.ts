7ba2c841c252adacf4af5f67d0810ffb7181bb0e
Chartok <Chartok@users.noreply.github.com>
2025-07-04 16:55:17 -0500

    added users router

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { User } from '../models/index.cjs';
+
+const router = Router();
+
+// Get all users
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      attributes: ['_id', 'username', 'email', 'savedBooks'],
+    });
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
