ebd1462aef2edba9ce8405d89227b0563268144c
Chartok <Chartok@users.noreply.github.com>
2025-07-04 22:47:23 -0500

    resolving conflicts

--- backend/src/migrations/20250704175417-create-user_saved_book-table.cjs
+++ backend/src/migrations/20250704175417-create-user_saved_book-table.cjs
@@ -39,9 +39,7 @@ module.exports = {
 
 	async down(queryInterface, Sequelize) {
 		await queryInterface.dropTable('user_saved_books'); // Drop the user_saved_books table
-		await queryInterface.dropTable('user_saved_books');
 		await queryInterface.sequelize.query(
-			'DROP TYPE IF EXISTS "enum_user_saved_books_shelf";'
 			'DROP TYPE IF EXISTS "enum_user_saved_books_shelf";'
 		); // Drop the ENUM type if it exists
 	},
