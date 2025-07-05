6b546e1e82ee077abceccee313f25eb4c7dfa6e7
Yaysa <Chartok@users.noreply.github.com>
2025-07-04 21:25:42 -0500

    ensuring environment vars line up

--- backend/src/db/dbConnection.ts
+++ backend/src/db/dbConnection.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const sequelize = new Sequelize(
 	process.env.DB_NAME || '',
-	process.env.DB_USER || '',
+	process.env.DB_USER || 'root',
 	process.env.DB_PASSWORD || '',
 	{
 		host: process.env.DB_HOST,
